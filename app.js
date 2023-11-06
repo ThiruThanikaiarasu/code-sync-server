a6f2f822c0cc5b1365da259db11e7ab7f520fc74
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-07 00:06:02 +0530

    Fix. Fixed the vercel.json.

--- app.js
+++ app.js
@@ -4,27 +4,26 @@ const app = express()
 const PORT = 3500
 
 const mongoose = require('mongoose')
-
-app.use(express.json())
-
 const cors = require('cors')
-app.use(cors())
-
 const signupRouter = require('./routes/signinRoute.js')
 
+app.use(express.json())
+app.use(cors())
+
+
+
 mongoose.connect(process.env.DB_URI)
-.then( () => {
-    app.listen(PORT, console.log(`Database is Connected and Server is running at http://localhost:3500/api/v1`))
-})
-.catch( (error) => {
-    console.log(error)
-})
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log("Connected to Database"))
 
 app.get('/', (request, response)=> {
     response.status(200).json({message:'CodeSync'})
 })
 
 app.use('/api/v1/signup', signupRouter)
+
+app.listen(PORT, console.log(`Server is running at http://localhost:3500/api/v1`))
 
 
 
